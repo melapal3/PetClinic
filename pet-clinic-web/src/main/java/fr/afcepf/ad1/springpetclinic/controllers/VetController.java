de2638eeba45d0eff08da1da01bb6c3101c8a9b6
melapal3 <melany.pallaro.03@gmail.com>
2020-07-17 16:12:08 +0200

    Added VetController and Vet list.html, Closes #6

---
+++ pet-clinic-web/src/main/java/fr/afcepf/ad1/springpetclinic/controllers/VetController.java
@@ -0,0 +1,22 @@
+package fr.afcepf.ad1.springpetclinic.controllers;
+
+import fr.afcepf.ad1.springpetclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    private final VetService vetService;
+
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/list", "vets/list.html/", "vets.html"})
+    public String listsVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/list";
+    }
+}
